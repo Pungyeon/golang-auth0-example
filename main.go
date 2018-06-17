9bfc92bb00a1f74ecea3d376d17c4631ebbb7849
Lasse Martin Jakobsen <lasse@tengen.dk>
2018-06-17 23:28:47 +0200

    working as intended copy paste from the quickstart, on the auth0 website

---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"golang.org/x/oauth2"
+)
+
+var Store *sessions.CookieStore
+
+func main() {
+
+	Store = sessions.NewCookieStore([]byte("something-very-secret"))
+	gob.Register(map[string]interface{}{})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/user", UserHandler)
+	r.HandleFunc("/login", LoginHandler)
+
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	domain := os.Getenv("AUTH0_DOMAIN")
+
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("AUTH0_CALLBACK_URL"),
+		Scopes:       []string{"openid", "profile"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://" + domain + "/authorize",
+			TokenURL: "https://" + domain + "/oauth/token",
+		},
+	}
+	state := r.URL.Query().Get("state")
+	session, err := Store.Get(r, "state")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if state != session.Values["state"] {
+		http.Error(w, "Invalid state parameter", http.StatusInternalServerError)
+		return
+	}
+
+	code := r.URL.Query().Get("code")
+
+	token, err := conf.Exchange(context.TODO(), code)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Getting now the userInfo
+	client := conf.Client(context.TODO(), token)
+	resp, err := client.Get("https://" + domain + "/userinfo")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var profile map[string]interface{}
+	if err = json.NewDecoder(resp.Body).Decode(&profile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session, err = Store.Get(r, "auth-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["id_token"] = token.Extra("id_token")
+	session.Values["access_token"] = token.AccessToken
+	session.Values["profile"] = profile
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Redirect to logged in page
+	http.Redirect(w, r, "/user", http.StatusSeeOther)
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	domain := os.Getenv("AUTH0_DOMAIN")
+	aud := os.Getenv("AUTH0_AUDIENCE")
+
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("AUTH0_CALLBACK_URL"),
+		Scopes:       []string{"openid", "profile"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://" + domain + "/authorize",
+			TokenURL: "https://" + domain + "/oauth/token",
+		},
+	}
+
+	if aud == "" {
+		aud = "https://" + domain + "/userinfo"
+	}
+
+	b := make([]byte, 32)
+	rand.Read(b)
+	state := base64.StdEncoding.EncodeToString(b)
+
+	// THIS IS NOT NECESSARY FOR THIS SIMPLE APPLICATION
+	session, err := Store.Get(r, "state")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	session.Values["state"] = state
+	err = session.Save(r, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	audience := oauth2.SetAuthURLParam("audience", aud)
+	url := conf.AuthCodeURL(state, audience)
+
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("USER HANDLER")
+	session, err := Store.Get(r, "auth-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(session)
+	fmt.Println(session.Values["profile"])
+	json.NewEncoder(w).Encode(session.Values["profile"])
+}
