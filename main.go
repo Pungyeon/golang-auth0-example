6c6be440d3f0175f99f4da3210152bd369c42be7
Lasse Martin Jakobsen <lasse@tengen.dk>
2018-07-11 16:11:35 +0200

    added an authRequired middleware route, to ensure that a token is present and valid, before handing out information from the todo list

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"net/http"
 	"path"
 	"path/filepath"
+	"time"
 
+	"github.com/gbrlsnchs/jwt"
 	"github.com/gin-gonic/gin"
 
 	"github.com/Pungyeon/golang-auth0-example/handlers"
 )
+
+// ValidateRequest will verify that a token received from an http request
+// is valid and signy by authority
+func authRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		jot, err := jwt.FromRequest(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, "cannot read token from request")
+			return
+		}
+
+		if jot.Validate(jwt.AlgorithmValidator("RS256")) != nil {
+			c.JSON(http.StatusUnauthorized, "could not validate signing algorithm")
+			return
+		}
+
+		if jot.Validate(jwt.IssuerValidator("https://pungy.eu.auth0.com/")) != nil {
+			c.JSON(http.StatusUnauthorized, "could not validate issuer")
+			return
+		}
+
+		if jot.Validate(jwt.ExpirationTimeValidator(time.Now())) != nil {
+			c.JSON(http.StatusUnauthorized, "token has expired")
+			return
+		}
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -23,10 +53,12 @@ func main() {
 		}
 	})
 
-	r.GET("/todo", handlers.GetTodoListHandler)
-	r.POST("/todo", handlers.AddTodoHandler)
-	r.DELETE("/todo", handlers.DeleteTodoHandler)
-	r.PUT("/todo", handlers.CompleteTodoHandler)
+	authorized := r.Group("/")
+	authorized.Use(authRequired())
+	authorized.GET("/todo", handlers.GetTodoListHandler)
+	authorized.POST("/todo", handlers.AddTodoHandler)
+	authorized.DELETE("/todo", handlers.DeleteTodoHandler)
+	authorized.PUT("/todo", handlers.CompleteTodoHandler)
 
 	err := r.Run(":3000")
 	if err != nil {
